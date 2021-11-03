07500cba71200076aabc7e0595088439091d4cd0
Charlie Malave <malavecharles@gmail.com>
2021-11-03 14:22:33 -0400

    added messenger component

---
+++ src/Containers/MessengerContainer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Messenger from "../Components/Messenger";
+const MessengerContainer = () => {
+return (
+    <div className="w-full  flex items-center justify-center bg-white-cream">
+     <Messenger />
+
+    </div>
+    ) 
+}
+
+export default MessengerContainer;
