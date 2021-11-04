7ac6a594d84588fc8be5d84a1a916d922db32379
Charlie Malave <malavecharles@gmail.com>
2021-11-03 20:48:29 -0400

    commiting new changes

---
+++ src/Containers/HomeContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeComponent from '../Components/HomeComponent'
+
+function HomeContainer() {
+    return (
+        <div>
+                <HomeComponent />
+        </div>
+    )
+}
+
+export default HomeContainer
---
+++ src/components/HomeComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeComponent = () =>{
+    return (
+        <div>
+            <h1> WELCOME TO CODEFORGE</h1>
+        </div>
+    )
+}
+
+export default HomeComponent
